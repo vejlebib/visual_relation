b6146e5a5843f27d9e9c9b07f70f5bcaf2c7aab2
Christian <bibchi@herning.dk>
2014-10-17 13:09:47 +0200

    Adjusted json-output to match ddb cms module

--- demo.php
+++ demo.php
@@ -1,9 +1,32 @@
 <?php
-
+	
 	require('ting.class.php');
 	
-	exit(
-		json_encode(
-			ting::get_record_as_array('870970-basis%3A27190731')
-		)
-	);
+	$id = '870970-basis:26917921';
+	
+	if (isset($_REQUEST['callback'])) {
+		
+		$callback = htmlspecialchars($_REQUEST['callback']);
+		
+		$record = ting::get_record_as_array($id);
+		
+		$property_set = [
+			'title' => 'dc:title',
+			'creator' => 'dc:creator+dkdcplus:aut',
+			'subject' => 'dc:subject+dkdcplus:DBCS',
+		];
+		
+		$properties = ['{"property":"id","value":' . json_encode($id) . '}'];
+		
+		foreach ($property_set as $property_key => $record_key) {
+			
+			if (isset($record[$record_key])) {
+				
+				foreach ($record[$record_key] as $value) {
+					$properties[] = '{"property":"' . $property_key . '","value":' . json_encode($value) . '}';
+				}
+			}
+		}
+		
+		exit($callback . '([' . implode(',', $properties) . ']);');
+	}
