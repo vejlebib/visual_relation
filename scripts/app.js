1627c27f6f83052477aa6f16aa6459d809664d59
Christian <bibchi@herning.dk>
2015-02-12 12:44:43 +0100

    Let VR handle buttons and load slide-buttons from webservice
    Using relvis.updateButtons() instead of relvis.open() to handle new
    buttons

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,13 @@ $(function() {
 
 	var cart = [];
 	var slideNo = 0;
-	var slideTimeout = 10000;
+	var slideTimeout = 300000;
 	var slideTimer = 0;
-
+	
 	function showAvailability(id) {
 		
 		$("#availability").html("");
-		(window.$ || window.jQuery).ajax("http://dev.vejlebib.dk/ting-visual-relation/get-availability/" + id + "?callback=?", {
+		(window.$ || window.jQuery).ajax("http://dev.vejlebib.dk/ting-visual-relation/get-availability/" + id, {
 			cache: true,
 			dataType: "jsonp",
 			success: function(data) {
@@ -59,17 +59,15 @@ $(function() {
 		showAvailability(o.id);
 		
 		// only show circular button if related objects exists
-		if (p.related) {
-			$("#details-circular").show();
-		}
-		else {
-			$("#details-circular").hide();
-		}
+		$("#details-circular").removeClass("relvis-enabled relvis-disabled").addClass("relvis-request").attr("data-relvis-id", o.id);
+		relvis.updateButtons();
 		
-		$("#details-circular").unbind("click").click(function(e) {
-			collapseDialogs();
-			relvis.open("circular", o.id);
-		});
+		//if (p.related) {
+		//	$("#details-circular").show();
+		//}
+		//else {
+		//	$("#details-circular").hide();
+		//}
 		
 		$("#details-external").unbind("click").click(function(e) {
 			collapseDialogs();
@@ -89,79 +87,118 @@ $(function() {
 		$("#details").show();
 	}
 	
-	// on every click the idle timeout is reset
-	$(document).click(function() {
+	function initVR() {
 		
-		clearInterval(slideTimer);
-		slideTimer = setInterval(showNextSlide, slideTimeout);
-	})
-
-	$("#bottom button").click(function() {
+		relvis.init({
+			apiUrl: "https://dev.vejlebib.dk/ting-visual-relation",
+			logUrl: "http://relvis.solsort.com/_relvis_log.js",
+			// relatedUrl: "http://relvis.solsort.com/relvis-api",
+			loadingCover: "images/wait.png",
+			disablePrefetch: false,
+			clickHandle: onObjectClick,
+			closeHandle: function() {},
+		});
 		
-		collapseDialogs();
-		$(this).addClass("active");
-	});
-
-	$("#details-close").click(function() {
+		// override the visual browsers margins to fit top & bottom bars
+		relvis.topMargin = 80;
+		relvis.bottomMargin = 80;
 		
-		$("#details").hide();
-	});
-	
-	// although never really submitting, we use the query forms submit so it can be triggered from keyboard
-	$("form").submit(function() {
-		
-		relvis.open("structural", "search:" + $("#query").val());
-		$("#query").val("");
-		collapseDialogs();
-		return false;
-	});
-
-	$("#cart-button").click(function() {
-		
-		$("#cart").toggle();
-	});
-
-	$("#email-button").click(function() {
-		
-		// faking emails for now
-		$("#msg").show();
-		setTimeout(function() { $("#msg").hide(); }, 3000);
-		resetCart();
-	});
-	
-	slideTimer = setInterval(showNextSlide, slideTimeout);
-
-	relvis.init({
-		apiUrl: "https://dev.vejlebib.dk/ting-visual-relation",
-		logUrl: "http://relvis.solsort.com/_relvis_log.js",
-		relatedUrl: "http://relvis.solsort.com/relvis-api",
-		loadingCover: "images/wait.png",
-		disablePrefetch: false,
-		clickHandle: onObjectClick,
-		closeHandle: function(){},
-	});
-	
-	// override the visual browsers margins to fit top & bottom bars
-	relvis.topMargin = 80;
-	relvis.bottomMargin = 80;
-	
-	// override the visual browsers drawBackground to remove its background + close-button & adjust styling
-	relvis.drawBackground = function(ctx, x, y, w, h) {
-		
-		if (relvis.getType() === "ext" && relvis.nodes.length >= 15) {
+		// override the visual browsers drawBackground to remove its background + close-button & adjust styling
+		relvis.drawBackground = function(ctx, x, y, w, h) {
 			
-			ctx.shadowBlur = relvis.unit / 3;
-			ctx.shadowColor = "#6eb5a8";
-			ctx.fillStyle = "#101a1c";
-			ctx.font = "700 italic " + relvis.unit * 3 + "px arial, sans-serif";
-			ctx.fillText("Forfatter", x + 4 * relvis.unit, y + 4 * relvis.unit);
-			var width = ctx.measureText("Anmeldelser").width;
-			ctx.fillText("Anmeldelser", x + w - width - 4 * relvis.unit, y + 4 * relvis.unit);
-			ctx.fillText("Emner", x + 4 * relvis.unit, y + h - 2 * relvis.unit);
-			width = ctx.measureText("Struktur").width;
-			ctx.fillText("Struktur", x + w - width - 4 * relvis.unit, y + h - 2 * relvis.unit);
-			ctx.shadowBlur = 0;
-		}
-	};
+			if (relvis.getType() === "ext" && relvis.nodes.length >= 15) {
+				
+				ctx.shadowBlur = relvis.unit / 3;
+				ctx.shadowColor = "#6eb5a8";
+				ctx.fillStyle = "#101a1c";
+				ctx.font = "700 italic " + relvis.unit * 3 + "px arial, sans-serif";
+				ctx.fillText("Forfatter", x + 4 * relvis.unit, y + 4 * relvis.unit);
+				var width = ctx.measureText("Anmeldelser").width;
+				ctx.fillText("Anmeldelser", x + w - width - 4 * relvis.unit, y + 4 * relvis.unit);
+				ctx.fillText("Emner", x + 4 * relvis.unit, y + h - 2 * relvis.unit);
+				width = ctx.measureText("Struktur").width;
+				ctx.fillText("Struktur", x + w - width - 4 * relvis.unit, y + h - 2 * relvis.unit);
+				ctx.shadowBlur = 0;
+			}
+		};
+	}
+	
+	function initSlides() {
+		
+		slideTimer = setInterval(showNextSlide, slideTimeout);
+	}
+	
+	function attachEvents() {
+		
+		// on every click the idle timeout is reset
+		$(document).click(function() {
+			
+			clearInterval(slideTimer);
+			slideTimer = setInterval(showNextSlide, slideTimeout);
+		})
+
+		$("#bottom button").click(function() {
+			
+			collapseDialogs();
+			$(this).addClass("active");
+		});
+		
+		$("#details-circular").click(function(e) {
+		 	collapseDialogs();
+		});
+
+		$("#details-close").click(function() {
+			
+			$("#details").hide();
+		});
+		
+		// although never really submitting, we use the query forms submit so it can be triggered from keyboard
+		$("form").submit(function() {
+			
+			relvis.open("structural", "search:" + $("#query").val());
+			$("#query").val("");
+			collapseDialogs();
+			return false;
+		});
+
+		$("#cart-button").click(function() {
+			
+			$("#cart").toggle();
+		});
+
+		$("#email-button").click(function() {
+			
+			// faking emails for now
+			$("#msg").show();
+			setTimeout(function() { $("#msg").hide(); }, 3000);
+			resetCart();
+		});
+	}
+
+	function loadConfig() {
+		
+		(window.$ || window.jQuery).ajax("https://dev.vejlebib.dk/ting-visual-relation/get-slideshow-settings", {
+			cache: true,
+			dataType: "jsonp",
+			success: function(data) {
+				slideTimeout = data.interval * 1000;
+				for (var i in data.slides) {
+					i = parseInt(i);
+					var type = data.slides[i].type;
+					var id = data.slides[i].datawell_pid;
+					var name = data.slides[i].name;
+					var query = data.slides[i].search_query;
+					$("#bottom").append('<button id="slide' + (i+1) + '" data-relvis-type="' + type + '" data-relvis-id="' + (query ? "search:" + query : id) + '" class="relvis-request">' + name + '</button>');
+				}
+				attachEvents();
+				initSlides();
+				initVR();
+			}
+		});
+		
+	}
+	
+	// start by loading config via ajax, which then inits the rest
+	loadConfig();
 	
 });
